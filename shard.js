0d18bef71296e02d248255ff3b466540c69c6983
Ritaline <87908327+Ritalincik@users.noreply.github.com>
2021-08-22 12:46:33 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,11 +1,11 @@
 const { ShardingManager } = require('discord.js');
 const config = require('./config.json')
-const sex = new ShardingManager('./bot.js', {
+const xd = new ShardingManager('./bot.js', {
 	totalShards: "auto",
     token: `${config.token}`
 });
-sex.spawn();
+xd.spawn();
 
-sex.on('shardCreate', shard => {
+xd.on('shardCreate', shard => {
     console.log(`[${new Date().toString().split(" ", 5).join(" ")}] #${shard.id+1} İDli shard başlatıldı.`);
 });
